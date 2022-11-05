733150d5ed7b73e4129f4fea270437f20db22b87
gotye.f@yandex.ru <gotye.f@yandex.ru>
2022-11-05 19:17:31 +0300

    1

---
+++ BotCH/ThreadHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotCH
+{
+    public class ThreadHelper
+    {
+        public static void Start(Thread thread)
+        {
+            if (thread.ThreadState == ThreadState.Suspended)
+            {
+                thread.Resume();
+            }
+            else
+            {
+                thread.Start();
+            }
+        }
+        public static void Stop(Thread thread)
+        {
+            if (thread.ThreadState == ThreadState.Running)
+            {
+                thread.Suspend();
+            }
+        }
+    }
+}
